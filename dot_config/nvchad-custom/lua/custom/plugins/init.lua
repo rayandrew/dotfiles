0b90d14bff0ca8b7fa9c1a407fd0b3a7d0415bc3
Ray Andrew <4437323+rayandrew@users.noreply.github.com>
2022-06-29 14:44:23 -0500

    add suda

--- dot_config/nvchad-custom/lua/custom/plugins/init.lua
+++ dot_config/nvchad-custom/lua/custom/plugins/init.lua
@@ -148,10 +148,10 @@ return {
   },
 
   ["lambdalisue/suda.vim"] = {
-    cmd = {
-      "SudaWrite",
-      "SudaRead",
-    },
+    -- cmd = {
+    --   "SudaWrite",
+    --   "SudaRead",
+    -- },
     config = function()
       vim.g.suda_smart_edit = 1
     end,
